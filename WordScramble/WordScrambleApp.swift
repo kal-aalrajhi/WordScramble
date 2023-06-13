23f5e66db0218fbd3e7b484a64f9ae116a8d3199
Kal <kal.aalrajhi@gmail.com>
2023-06-13 12:41:09 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dr Cpt Blackbeard on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
